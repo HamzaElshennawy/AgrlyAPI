84359d2a35866fe6df63e0f697a40b753a5fc90b
HamzaElshennawy <elshennawyhamza@gmail.com>
2025-03-01 12:26:07 +0200

    Add project files.

---
+++ AgrlyAPI/Controllers/Users/usersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgrlyAPI.Controllers.users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usersController : ControllerBase
+    {
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok( "Hello, World!" );
+		}
+	}
+}
---
+++ AgrlyAPI/Controllers/docs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgrlyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class docs : ControllerBase
+    {
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Redirect( "/swagger" );
+		}
+	}
+}
